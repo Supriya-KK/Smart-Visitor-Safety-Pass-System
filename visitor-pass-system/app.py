12c749eb47753af2d8528f6d12dbf40cd3fe505b
Mayuri0495 <mayuripatil.2044@gmail.com>
2025-06-23 16:51:39 +0530

    added home page

--- visitor-pass-system/app.py
+++ visitor-pass-system/app.py
@@ -52,6 +52,10 @@ def update_schema():
 
 @app.route('/')
 def home():
+    return render_template('index.html')
+
+@app.route('/register')
+def register():
     return render_template('form.html')
 
 @app.route('/about')
